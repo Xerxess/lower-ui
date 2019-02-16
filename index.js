9a2caca97c85e25548ef71269c709a87f9a10b6e
unknown <969371501@qq.com>
2019-02-16 10:54:08 +0800

    提交

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ import _style from './demo/style.css';
 import _style2 from './demo/style.mod.css?module';
 import myTransition, { vendor } from './demo/myTransition.js';
 import ScrollReveal from 'scrollreveal';
+import { Swiper, Navigation, Pagination, Scrollbar } from 'swiper/dist/js/swiper.esm.js';
+import 'swiper/dist/css/swiper.css';
 import './demo/index.less';
 
+Swiper.use([Navigation, Pagination, Scrollbar]);
 
 function component() {
     var element = document.createElement('div');
@@ -25,10 +28,53 @@ window.onload = function () {
     console.log((start - end).toFixed(3)) // ~ 0.002 on my system
     console.log(window.performance.now());
 
-    ScrollReveal().reveal('.scrollreveal__item');
+    ScrollReveal({ reset: true }).reveal('.scrollreveal__item');
+    var swiper = new Swiper('.swiper-container', {
+        speed: 500,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        // ...
+    });
 }
 
 document.body.appendChild(component());
 
 console.log(new myTransition());
 console.log(_style);
+
+
+//普通
+function add(x, y) {
+    return (x + y)
+}
+
+//柯里化
+function curriedAdd(x) {
+    return function (y) {
+        return x + y
+    }
+}
+
+//通过递归来将 currying 的返回的函数也自动 Currying 化
+function trueCurrying(fn, ...args) {
+    if (args.length >= fn.length) {
+        return fn(...args)
+    }
+    return function (...args2) {
+        return trueCurrying(fn, ...args, ...args2)
+    }
+}
+var increment = trueCurrying(add, 1);
+console.log(increment(1));
+
+// add(1)(2) // 3 
+// add(1, 2, 3)(10) // 16 
+// add(1)(2)(3)(4)(5) // 15
+
+var addd = function (...args) {
+    return function () {
+
+    }
+}
